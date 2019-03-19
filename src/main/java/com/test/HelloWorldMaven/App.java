d6670f90d29e9e13c6546ab54b6d3882304df731
AJ1804 <48342500+AJ1804@users.noreply.github.com>
2019-03-19 08:39:52 +0530

    Update App.java

--- src/main/java/com/test/HelloWorldMaven/App.java
+++ src/main/java/com/test/HelloWorldMaven/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello INDIA " );
+        System.out.println( "Devesh is crazy frog" );
     }
 }
